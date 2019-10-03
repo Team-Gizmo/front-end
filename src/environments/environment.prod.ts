ad55cbeff3e1c616a23893558b21965a83237147
lfredric <lfredrickson@paychex.com>
2019-10-03 12:50:16 -0400

    Update to display Gizmo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   backendUrlOld: 'http://dev-d-116g1.paychex.com:9090/sandbox-1.0/api/',
-  backendUrl: 'http://localhost:9090/sandbox-1.0/api/',
+  backendUrl: 'http://dev-d-116g1.paychex.com:9090/sandbox-1.0/api/',
   incBackendUrl: 'https://paychex.service-now.com/nav_to.do?uri=incident.do?sys_id='
 };
