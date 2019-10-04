ede9b053c0cfb1661fd6165b93006f02b3a3ae5c
lfredric <lfredrickson@paychex.com>
2019-10-04 07:45:50 -0400

    Update url references

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendUrlOld: 'http://dev-d-116g1.paychex.com:9090/sandbox-1.0/api/',
-  backendUrl: 'http://dev-d-116g1.paychex.com:9090/sandbox-1.0/api/',
+  backendUrlOld: 'http://dev-d-116g1.paychex.com:9090/gizmo/api/',
+  backendUrl: 'http://dev-d-116g1.paychex.com:9090/gizmo/api/',
   incBackendUrl: 'https://paychex.service-now.com/nav_to.do?uri=incident.do?sys_id='
 };
